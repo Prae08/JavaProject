ec9a15356fa3babc658cb81f44c922ff705131e4
Prae08 <sasi.prae2004@gmail.com>
2024-07-05 10:44:17 +0700

    This is 3rd status

--- test01.java
+++ test01.java
@@ -6,5 +6,6 @@ public class test01 {
     public static void main(String[] args) {
         System.out.println("Hello World");
         System.out.println("A cat is on a mat");
+        System.out.println("A cat is red");
     }
 }
