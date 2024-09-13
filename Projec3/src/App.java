19f632bc976c87f7b1a4056ef28ffa8bec137d93
Sasi <sasi.prae2004@gmail.com>
2024-09-13 21:21:47 +0700

    Delete Projec3 directory

--- Projec3/src/App.java
+++
@@ -1,7 +0,0 @@
-public class App {
-    public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
-        Student student = new Student("Jonh Wick", "Biplogy", 35);
-        student.displayInfo();
-    }
-}
